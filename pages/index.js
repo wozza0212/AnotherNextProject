41a366f8f0357c640d6fd195ddc4643f9600131d
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-08-19 20:29:03 +0100

    added getstaticprops

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,18 @@ import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+const Home = ({ allPostsData }) => {
   return (
     <Layout home>
       <div className={styles.container}>
@@ -12,11 +22,36 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Hey, I'm Tom Wasnidge! I currently work as an Asscociate Engineer for Sainsbury's,
+          This website is a small portfolio WebApp built using the NextJS framework!
+          I'm building this site in order to get better at web development and hopefully
+          learn and experiment with a few different technologies! I'm hoping if I start to write about 
+          thing then I'll start to learn them quicker!
+          Anyways, I hope you enjoy!
+        </p>
+        <br />
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+        {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        <Link href="/posts/new-post">
+          <h3>First Post</h3>
+        </Link>
       </section>
 
     </div>
@@ -24,3 +59,5 @@ export default function Home() {
 
   )
 }
+
+export default Home;
