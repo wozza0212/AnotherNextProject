b7b407fe674f92a4a5806113cea382b5958653e7
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-08-21 18:08:50 +0100

    added some navbar links

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./navbar.module.css";
+import Link from "next/link";
 
 const NavBar = () => { 
     return (
@@ -8,12 +9,12 @@ const NavBar = () => {
                     <Image src="/alt-menu-svgrepo-com.svg" height={30} width={30} alt="hamburger menu" />
                 </div>
                 <div className={styles.logo}>
-                    <a><h1 className={styles.logoheading}>NextJS Blog</h1></a>
+                    <Link href='/'><h1 className={styles.logoheading}>NextJS Blog</h1></Link>
                 </div>
                 <ul>
-                    <a><li>Home</li></a>
-                    <a><li>About</li></a>
-                    <a><li>NextJS</li></a>
+                    <Link href='/'><li>Home</li></Link>
+                    <Link href='/about/'><li>About</li></Link>
+                    <Link href='/posts'><li>Blog</li></Link>
                 </ul>
             </nav>
     )
---
+++ pages/posts/index.js
@@ -0,0 +1,13 @@
+import Layout from "../../components/layout";
+
+const Blog = () => {
+    return (
+        <Layout>
+            <div>
+                <h1>Blog</h1>
+            </div>
+        </Layout>
+    )
+ }
+
+export default Blog;
