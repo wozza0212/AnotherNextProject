9fffcadfbf2099cf47a8a486dc8694a09125dc6b
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-08-22 21:28:45 +0100

    added blog css, copied from utilstyles

--- components/blog/blog.js
+++ components/blog/blog.js
@@ -1,18 +1,17 @@
 import Link from "next/link";
-import Layout from "../layout";
 import Date from "../date";
-import utilStyles from "../../styles/utils.module.css";
+import styles from "./blog.module.css";
 
 const Blog = ({ allPostsData }) => {
     return (
-            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-            <h2 className={utilStyles.headingLg}>Blog</h2>
-                <ul className={utilStyles.list}>
+            <section className={`${styles.headingMd} ${styles.padding1px}`}>
+            <h2 className={styles.headingLg}>Blog</h2>
+                <ul className={styles.list}>
                 {allPostsData.map(({ id, date, title }) => (
-                    <li className={utilStyles.listItem} key={id}>
+                    <li className={styles.listItem} key={id}>
                     <Link href={`/posts/${id}`}>{title}</Link>
                     <br />
-                    <small className={utilStyles.lightText}>
+                    <small className={styles.lightText}>
                         <Date dateString={date} />
                     </small>
                 </li>
